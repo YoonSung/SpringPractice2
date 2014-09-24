f8be0e120a0d3275d4136f9b7514c3260972c272
Yoonsung <lvev9925@naver.com>
2014-09-24 21:25:27 +0900

    ViewResolver 설정. jsp파일 은닉화

--- SecondPractice/src/main/java/spring/practice/controller/HomeController.java
+++ SecondPractice/src/main/java/spring/practice/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("test", System.currentTimeMillis());
-		return "home.jsp";
+		return "home";
 	}
 }
