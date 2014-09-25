6bfed0290dcf0643a47a43a8307f83e3ee592b38
Yoonsung <lvev9925@naver.com>
2014-09-25 15:14:49 +0900

    Annotation기반의 Alias 설정 테스트적용

--- SecondPractice/src/main/java/spring/practice/domain/User.java
+++ SecondPractice/src/main/java/spring/practice/domain/User.java
@@ -2,9 +2,11 @@ package spring.practice.domain;
 
 import javax.validation.constraints.Size;
 
+import org.apache.ibatis.type.Alias;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+@Alias("Test")
 public class User {
 	
 	@NotEmpty
